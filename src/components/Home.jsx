fbe93692f525ca9d3714c10c1601319a8519cf69
dagmfre <dagmfre@gmail.com>
2024-05-04 01:24:27 +0300

    import Header and add background image

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <img className="back-img" src="back_img.jpeg" alt="" />
+    </div>
+  );
+}
