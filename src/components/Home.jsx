0f5b743df16c3f8709e6de0ed73ddce8dc3c1a62
dagmfre <dagmfre@gmail.com>
2024-05-11 11:56:33 +0300

    Import and add components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,19 @@
 import Header from "./Header";
+import FrontPage from "./FrontPage";
+import About from "./About";
+import Mission from "./Mission";
+import Partners from "./Partners";
+import Team from "./Team";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <Header />
-      <img className="back-img" src="back_img.jpeg" alt="" />
+      <FrontPage />
+      <About />
+      <Mission />
+      <Partners />
+      <Team />
     </div>
   );
 }
