3d1fc6554d7970e03829be822df07ba94690fa89
dagmfre <dagmfre@gmail.com>
2024-10-14 02:02:00 +0300

    Refactor Contacts component to add email functionality and display success/error messages

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "@mui/material/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Alert from "@mui/material/Alert";
+import emailjs from "@emailjs/browser";
 
 export default function Contacts() {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
-    email: "",
+    user_email: "",
     phoneNumber: "",
     message: "",
   });
+  const [isSent, setIsSent] = useState(false);
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const formRef = useRef();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -19,8 +25,40 @@ export default function Contacts() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form data:", formData);
-    setFormData({ name: "", email: "", phoneNumber: "", message: "" });
+    setSuccessMessage("");
+    setError("");
+
+    emailjs
+      .sendForm(
+        "service_mrs2gmr",
+        "template_1atvw44",
+        formRef.current,
+        "2vAafyWokU5vCYmE9"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsSent(true);
+          setSuccessMessage("Your message has been sent successfully!");
+
+          setFormData({
+            firstName: "",
+            lastName: "",
+            user_email: "",
+            phoneNumber: "",
+            message: "",
+          });
+
+          setTimeout(() => {
+            setIsSent(false);
+            setSuccessMessage("");
+          }, 3000);
+        },
+        (error) => {
+          console.error("Failed to send email:", error.text);
+          setError("Failed to send your message. Please try again.");
+        }
+      );
   };
 
   return (
@@ -30,13 +68,25 @@ export default function Contacts() {
           <h1>Get in touch!</h1>
         </div>
 
-        <img src="emsa-circle-img.png" alt="" />
+        <img src="/emsa-circle-img.png" alt="" />
       </div>
+
       <div className="contact-form">
         <h1>Contact us</h1>
-        <form onSubmit={handleSubmit}>
+        {isSent && successMessage && (
+          <Alert severity="success" sx={{ width: "100%", mt: 2 }}>
+            {successMessage}
+          </Alert>
+        )}
+
+        {error && (
+          <Alert severity="error" sx={{ width: "100%", mt: 2 }}>
+            {error}
+          </Alert>
+        )}
+        <form ref={formRef} onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="name">First Name:</label>
+            <label htmlFor="firstName">First Name:</label>
             <input
               type="text"
               id="firstName"
@@ -48,7 +98,7 @@ export default function Contacts() {
             />
           </div>
           <div>
-            <label htmlFor="email">Last Name:</label>
+            <label htmlFor="lastName">Last Name:</label>
             <input
               type="text"
               id="lastName"
@@ -64,9 +114,9 @@ export default function Contacts() {
             <input
               type="email"
               id="email"
-              name="email"
+              name="user_email"
               placeholder="Enter your email"
-              value={formData.email}
+              value={formData.user_email}
               onChange={handleInputChange}
               required
             />
@@ -95,6 +145,7 @@ export default function Contacts() {
             />
           </div>
           <Button
+            type="submit"
             variant="contained"
             endIcon={<FontAwesomeIcon icon={faArrowRight} />}
             sx={{
