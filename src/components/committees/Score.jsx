8409a9eb0d65f6c0fcd209a0923a9b61305be015
dagmfre <dagmfre@gmail.com>
2024-10-14 02:12:47 +0300

    Refactor image source paths in Score component

--- src/components/committees/Score.jsx
+++ src/components/committees/Score.jsx
@@ -1,5 +1,18 @@
 import Header from "../Header";
 import Footer from "../Footer";
+import {
+  Box,
+  Container,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import Groups2Icon from "@mui/icons-material/Groups2";
+import PublicIcon from "@mui/icons-material/Public";
+import SchoolIcon from "@mui/icons-material/School";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 
 export default function Score() {
   return (
@@ -8,16 +21,13 @@ export default function Score() {
       <div className="the-committee-front">
         <img src="/research1.svg" alt="" />
         <img src="/research2.svg" alt="" />
-        <img src="/SCORE1.png" alt="" />
+        <img src="/SCORE.png" alt="" />
         <h1>Research Exchanges (SCORE)</h1>
         <p>
-          Unfortunately, many medical students do not have the opportunity to
-          experience research while in medical school. Research is essential for
-          the development of medicine and without it, the medical field could
-          never have reached the stage it is at now with innovative methods of
-          diagnosis and treatments. Put together the experience of improving
-          research skills with an unforgettable cultural learning opportunity
-          and you have the Standing Committee on Research Exchange (SCORE)!
+          SCORE is the standing committee on research exchange which is one of
+          the six standing committees of the IFMSA. EMSA Ethiopia's SCORE has
+          been in office for more than 5 years now. The main focus area are
+          Research and Research exchanges.
         </p>
       </div>
       <div className="committee-history">
@@ -29,12 +39,12 @@ export default function Score() {
         </div>
         <div>
           <p>
-            SCORE was founded in 1991 with the objective of giving students an
-            opportunity of improving their skills in research in other learning
-            settings. Presently, SCORE involves more than 65 active NMOs,
-            offering over 3000 research projects to provide over 2400 medical
-            students worldwide the opportunity to participate in IFMSA research
-            exchange program
+            SCORE Ethiopia has been handed down through many generations. The
+            current NORE is Dr. Abigael Abiy Mesfin. In 2023, SCORE Ethiopia was
+            activated in IFMSA during Dr. Sauel Mesfin Girma's term as the
+            National officer on Research Exchange. In the year 2024, SCORE
+            Ethiopia started its first research exchanges with around 18
+            outgoing exchange students.
           </p>
           <p>
             and learn the basic principles of medical research such as
@@ -54,69 +64,115 @@ export default function Score() {
             </h4>
           </div>
           <p>
-            Our mission is to offer future physicians an opportunity to
-            experience research and diversity in countries all over the world.
-            This is achieved by providing a network of locally and
-            internationally active students that globally facilitate access to
-            research exchange projects. Through our programming and
-            opportunities, we aim to develop both culturally sensitive students
-            and skilled researchers intent on shaping the world of science.
+            Research is a crucial part of any education especially medicine.
+            Therefore, We believe medical students should have the necessary
+            exposure to research as medical students. Our vision is for research
+            to be adequately included as part of the medical curricula and for
+            medical students to be competent in addition to their academics. Our
+            mission is to improve the access and engagement of medical students
+            to research and research education. To cultivate medical students'
+            passion and enthusiasm on research
           </p>
         </div>
         <div className="objectives-cont">
           <div className="objectives">
+            <img src="/score-icon1.svg" alt="" />
+            <div>
+              <h4>Research Exchanges</h4>
+              <p>
+                Promote research exchanges locally and internationally to foster
+                collaboration and knowledge sharing.
+              </p>
+            </div>
+          </div>
+          <div className="objectives">
             <img src="/score-icon2.svg" alt="" />
             <div>
-              <h4>Self-Learning</h4>
-              <p>
-                Enable them to take responsibility for their own learning
-                according to their personal interests.
-              </p>
-            </div>
-          </div>
-          <div className="objectives">
-            <img src="/score-icon1.svg" alt="" />{" "}
-            <div>
               <h4>Research Basics</h4>
-              <p>Introduce them to the basic principles of medical research</p>
-            </div>
-          </div>
-          <div className="objectives">
-            <img src="/score-icon3.svg" alt="" />{" "}
-            <div>
-              <h4>Broaden Views</h4>
               <p>
-                Widen their horizons and provide the opportunity to experience
-                different approaches to health care, ethical research standards,
-                medical research, education, and treatment.
+                Ensure medical students know the basics of how to do research,
+                providing them with essential skills for their future careers.
               </p>
             </div>
           </div>
           <div className="objectives">
-            <img src="/score-icon5.svg" alt="" />{" "}
+            <img src="/score-icon3.svg" alt="" />
             <div>
-              <h4>Improve Education</h4>
+              <h4>Career Options</h4>
               <p>
-                Enhance the academic quality of the medical student curricula
-                and achieve educational benefits of practical and theoretical
-                knowledge in the field of medical research.
+                Highlight career options in medical research to inspire and
+                guide students towards various professional paths in the field.
               </p>
             </div>
           </div>
           <div className="objectives">
-            <img src="/score-icon4.svg" alt="" />{" "}
+            <img src="/score-icon5.svg" alt="" />
             <div>
-              <h4>Global Collaboration</h4>
+              <h4>Research Culture</h4>
               <p>
-                Facilitate collaboration and partnership between medical
-                universities, research institutions and allied medical students
-                across the globe in order to share and spread new achievements
-                in medical research.
+                Influence the research culture in hospitals to involve the young
+                generation, encouraging their active participation and
+                innovation.
               </p>
             </div>
           </div>
         </div>
       </div>
+
+      <div className="score committee-achievements">
+        <Box display="flex" alignItems="center" mb={2}>
+          <EmojiEventsIcon
+            style={{
+              width: "3rem",
+              height: "3rem",
+              backgroundColor: "#0000001a",
+              padding: "8px",
+              borderRadius: "50%",
+              border: "1px solid #00000052",
+            }}
+          />
+          <Typography variant="h4" fontWeight={700}>
+            Our Achievements
+          </Typography>
+        </Box>
+        <Container>
+          <Typography variant="body1" paragraph>
+            SCORE has achieved significant contributions to research education,
+            international collaboration, and organizational partnerships.
+          </Typography>
+          <List
+            sx={{
+              width: "100%",
+              bgcolor: "background.paper",
+              maxWidth: "initial",
+            }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+          >
+            <ListItemButton>
+              <ListItemIcon>
+                <SchoolIcon />
+              </ListItemIcon>
+              <ListItemText primary="Conducted nation-wide research education training series" />
+            </ListItemButton>
+
+            <ListItemButton>
+              <ListItemIcon>
+                <PublicIcon />
+              </ListItemIcon>
+              <ListItemText primary="Hosted international research exchanges in our country for the first time" />
+            </ListItemButton>
+
+            <ListItemButton>
+              <ListItemIcon>
+                <Groups2Icon />
+              </ListItemIcon>
+              <ListItemText primary="Built partnerships with research organizations" />
+            </ListItemButton>
+          </List>
+        </Container>
+      </div>
+
       <Footer />
     </div>
   );
