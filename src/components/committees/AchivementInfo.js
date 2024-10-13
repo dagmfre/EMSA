2047e9d152efc4e7135e20016a94aa0f3f58c9f2
dagmfre <dagmfre@gmail.com>
2024-10-14 02:10:53 +0300

    Add AchivementInfo component to store and display achievements information

---
+++ src/components/committees/AchivementInfo.js
@@ -0,0 +1,93 @@
+const achievementsInfo = {
+  scorp: [
+    {
+      title: "ለወገኖቻችን እጃችን እንዘርጋ PROJECT",
+      description:
+        "Due to the current situation of our country, many people are forced to leave their homes and settle at a camp in Debrebrhan. There are about 7000 to 16000 people at a Single camp. As follows, we were able to collect and donate clothes, shoes, sanitary pads and face masks from medical students at different campuses.",
+    },
+    {
+      title: "PROJECT ASKWALA",
+      description:
+        "In collaboration with Memhr Weldemariyam Charity Association, we have collected and donated books to Mehal Amba primary school found in the rural area of Debrebirhan. Any books from fictions to school books are being collected.",
+    },
+    {
+      title: "HOT ISSUE",
+      description:
+        "We believe every child deserve to be happy. Last Christmas, we gave toys and clothes to children living on the streets of Addis.",
+    },
+    {
+      title: "PROJECT WECARE",
+      description:
+        "Weekly training that lasted for 4 weeks and was given at Mekanisa Deaf School. We trained the students on Sexual and reproductive health, Addiction and Advocacy.",
+    },
+    {
+      title: "LEARNING DISABILITY PROJECT",
+      description:
+        "Our Learning Disability Awareness Project collaborated with FANA Ethiopia to train and certify over 70 health professionals. Through an awareness training session, we equipped participants with essential knowledge on diagnosing and treating learning disabilities. Additionally, in partnership with Yetenaweg, we conducted a podcast session to further raise awareness on this important topic. Together, we worked towards promoting inclusive care and enhancing understanding of learning disabilities among healthcare providers and the broader community.",
+    },
+    {
+      title: "Disability Inclusive Health Care System",
+      description:
+        "Our project focused on promoting disability-inclusive healthcare through an online seminar. We organized a thought-provoking discussion with Zewdu Guluma, a disability mainstreaming and sign language specialist from the Ministry of Health. The session highlighted critical issues that require immediate interventions within the healthcare system. By facilitating this informative online seminar, we aimed to raise awareness and advocate for urgent actions to create a more inclusive healthcare environment for individuals with disabilities.",
+    },
+    {
+      title: "Children's Heart Fund Ethiopia STUDENT AMBASSADORSHIP PROGRAM",
+      description:
+        "Children's Heart Fund Ethiopia STUDENT AMBASSADORSHIP PROGRAM",
+    },
+  ],
+  scora: [
+    {
+      title: "One Stop Project",
+      description:
+        "Led by LCs under EMSA Ethiopia, this initiative supported sexual assault survivors at the 'One Stop' center in Gandhi Memorial Hospital. Essential items such as sanitary pads, menstrual cups, clothing, soaps, detergents, and learning materials were donated. Fundraising events and collaborations with organizations and medical staff helped achieve donation goals. The project ran from April 22, 2023, to June 3, 2023, under the guidance of Dr. Ismael Aminu.",
+    },
+    {
+      title: "Pillars of SRHR Forum",
+      description:
+        "Organized by SCORA Ethiopia in collaboration with EngenderHealth, this forum focused on sexual and reproductive health rights (SRHR) in Ethiopia. It identified gaps in healthcare access and facilitated discussions among 160 participants, primarily medical students, enhancing awareness and collaboration on SRHR initiatives.",
+    },
+    {
+      title: "16 Days of Activism Against GBV",
+      description:
+        "On December 2, 2023, a panel discussion on 'Interpersonal Relationships for Young Medical Professionals' emphasized the importance of consent and healthy relationships. Supported by EngenderHealth, the event encouraged medical students to reflect on building healthy relationships to combat GBV.",
+    },
+    {
+      title: "Project Enes Le Wegene",
+      description:
+        "Enrolled under IFMSA's program for HIV, AIDS, and STI, this project was organized by SCORA-HMC in collaboration with SCORA-MMC, SCORA-ECUSTA, and Zewditu Memorial Hospital. The initiative provided HIV counseling and testing for marginalized communities. In Phase 1, 20 members of the Ethiopian National Association of the Blind received voluntary testing. Phase 2 expanded to Wereda 7, engaging underprivileged women in health discussions through an Ethiopian Coffee Ceremony. Both phases were supported by Zewditu Memorial Hospital, offering follow-up care for those in need.",
+    },
+    {
+      title: "Breast Cancer Awareness Campaign",
+      description:
+        "Conducted by SCORA-MMC from October 26-28, 2023, this campaign involved distributing brochures to 400 patients at OPDs and promoting the 'Dear Mama' breast self-examination app. The event highlighted the power of partnerships in advocating for better health outcomes.",
+    },
+    {
+      title: "SCORA-AA Blood Donation Drive",
+      description:
+        "In collaboration with Wings of Healing, SCORA-AA organized a blood donation drive for Cervical Cancer Awareness Month, significantly contributing to the fight against cervical cancer through community engagement.",
+    },
+    {
+      title: "Mereja Breastfeeding Digital Manual",
+      description:
+        "SCORA-ECUSTA released the 'Mereja Breastfeeding Digital Manual' in Amharic and Afaan Oromo, providing mothers with essential breastfeeding information. This manual was distributed at Gandhi Memorial Hospital, reaching many families and promoting maternal and child health.",
+    },
+    {
+      title: "SRHR Training for Deaf Students",
+      description:
+        "SCORA-AA conducted an SRHR training program for students at Mekanisa Deaf School, covering topics such as HIV/AIDS, contraception, menstrual health, and safe abortion. The program empowered students with crucial knowledge for informed reproductive health choices.",
+    },
+    {
+      title: "Safe Abortion Dialogue and Movie Night",
+      description:
+        "SCORA-AA hosted a dialogue session and movie night focused on safe abortion and reproductive rights. Participants engaged in discussions, sharing experiences and insights, and promoting awareness of SRHR issues within the community.",
+    },
+    {
+      title: "Cervical Cancer Awareness Campaign",
+      description:
+        "SCORA-JU organized a successful cervical cancer awareness campaign, distributing over 100 brochures and engaging 50 volunteers. The campaign emphasized collaborative efforts with Jimma University’s gynecology and obstetrics departments to promote early detection and prevention.",
+    },
+  ],
+};
+
+export default achievementsInfo;
