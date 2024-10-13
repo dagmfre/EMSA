5440f8e84ec38eacd517b7f1aabca75517d1fcfa
dagmfre <dagmfre@gmail.com>
2024-10-14 02:08:10 +0300

    Refactor image source paths in Partners component

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,22 +5,22 @@ export default function Partners() {
       <div className="partners-background"></div>
       <div className="partners-logo-cont">
         <Link data-aos-once="true" data-aos="slide-right" target="_blank" to={"https://www.ipas.org/"}>
-          <img src="ipas.webp" alt="" />
+          <img src="/ipas.webp" alt="" />
         </Link>
         <Link data-aos-once="true" data-aos="slide-right" target="_blank" to={"https://www.jhpiego.org/"}>
-          <img src="jhpiego.svg" alt="" />
+          <img src="/jhpiego.svg" alt="" />
         </Link>
         <Link data-aos-once="true" data-aos="slide-right" target="_blank" to={"https://www.who.int/"}>
-          <img src="who.svg" alt="" />
+          <img src="/who.svg" alt="" />
         </Link>
         <Link data-aos-once="true" data-aos="slide-right" target="_blank" to={"https://www.ethiopianmedicalass.org/"}>
-          <img src="ema.png" alt="" />
+          <img src="/ema.png" alt="" />
         </Link>
         <Link data-aos-once="true" data-aos="slide-right" target="_blank" to={"https://www.engenderhealth.org/"}>
-          <img src="engender.svg" alt="" />
+          <img src="/engender.svg" alt="" />
         </Link>
         <Link data-aos-once="true" data-aos="slide-right" target="_blank" to={"https://ifmsa.org/"}>
-          <img src="ifmsa.png" alt="" />
+          <img src="/ifmsa.png" alt="" />
         </Link>
       </div>
       <div className="partners-background"></div>
