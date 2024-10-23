99b52c1ae253f06e3bf63a8ee87e6eb35fac692d
dagmfre <dagmfre@gmail.com>
2024-10-24 00:52:44 +0300

    Refactor Footer component to update social media links and add developer portfolio link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,100 +1,105 @@
-import emailjs from "@emailjs/browser";
-import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faTelegram } from "@fortawesome/free-brands-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
-  const [isSent, setIsSent] = useState(false);
-  const form = useRef();
+  const location = useLocation();
+  const isHome = location.pathname === "/";
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_mrs2gmr",
-        "template_1atvw44",
-        form.current,
-        "2vAafyWokU5vCYmE9"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setIsSent(!isSent);
-          setTimeout(() => {
-            if (result.status === 200) {
-              setIsSent(false);
-            }
-          }, 3000);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  const handleScrollToExecutives = () => {
+    const element = document.getElementById("executives");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
   };
-  return (
-    <div className="footer">
-      <div className="footer-childs">
-        <h1>EMSA</h1>
-        <p>
-          EMSA is not-for-profit, students-based organization established for
-          empowering medical students and advance health care delivery in
-          Ethiopia.
-        </p>
-        <div className="info-cont">
-          <i class="fa-solid fa-square-phone"></i>
-          <p>+251989999055</p>
-        </div>
-        <div className="info-cont">
-          <FontAwesomeIcon icon={faEnvelope} />
-          <p>emsa-ethiopia@ifmsa.org</p>
-        </div>
 
-        <div className="social-cont">
-          <FontAwesomeIcon icon={faFacebook} />
-          <FontAwesomeIcon icon={faTelegram} />
-          <FontAwesomeIcon icon={faTwitter} />
-          <FontAwesomeIcon icon={faYoutube} />
+  return (
+    <div className={`footer`}>
+      <div className="footer-child-cont">
+        <div className="footer-childs">
+          <h1>EMSA</h1>
+          <p>
+            EMSA is not-for-profit, students-based organization established for
+            empowering medical students and advance health care delivery in
+            Ethiopia.
+          </p>
+          <div className="info-cont">
+            <i className="fa-solid fa-square-phone"></i>
+            <p>+251989999055</p>
+          </div>
+          <div className="info-cont">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <p>emsa-ethiopia@ifmsa.org</p>
+          </div>
+        </div>
+        <div className="footer-childs">
+          <h1>Association</h1>
+          <Link to="/about">About Us</Link>
+          <Link to="/local-chapters">Local Chapters</Link>
+          <Link to="/committees">Standing Committees</Link>
+          <Link to="#executives" onClick={handleScrollToExecutives}>
+            Executives
+          </Link>
+          <Link to="/join-us">How You Can Join</Link>
+        </div>
+        <div className="footer-childs">
+          <h1>WORKING HOURS</h1>
+          <p>Mon - Fri: 9:00AM - 9:00PM</p>
+          <p>Sat: 9:00AM - 19:00PM</p>
+          <p>Sun: Closed</p>
+        </div>
+        <div className="footer-childs">
+          <h1>Get In Touch</h1>
+          <p>Connect with us through our social media channels:</p>
+          <div className="social-cont">
+            <Link to={"https://et.linkedin.com/company/emsa-ethiopia"}>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </Link>
+            <Link to={"https://t.me/s/EMSA_ETHIOPIA"}>
+              <FontAwesomeIcon icon={faTelegram} />
+            </Link>
+            <Link to={"https://www.instagram.com/emsa_ethiopia"}>
+              <FontAwesomeIcon icon={faInstagram} />
+            </Link>
+            <Link to={"https://www.facebook.com/youEMSA"}>
+              <FontAwesomeIcon icon={faFacebook} />
+            </Link>
+          </div>
         </div>
       </div>
-      <div className="footer-childs">
-        <h1>Association</h1>
-        <a href="#/">Local Chapters</a>
-        <a href="#/">Standing Committees</a>
-        <a href="#/">Services</a>
-        <a href="#/">Achievements</a>
-        <a href="#/">How You Can Join</a>
+      <hr
+        style={{
+          maxWidth: "1450px",
+          alignSelf: "center",
+          width: "100%",
+          margin: "-2rem 0 1rem",
+          // display: isHome ? "initial" : "none",
+        }}
+      />
+      <div
+        className="footer-bottom"
+        // style={{
+        //   display: isHome ? "flex" : "none",
+        // }}
+      >
+        <p>&copy; {new Date().getFullYear()} EMSA. All rights reserved.</p>
+        <p className="portfolio-link">
+          Designed & developed by &nbsp;
+          <Link
+            to={"https://dagmfre-seid.vercel.app/"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @Dagmfre Seid
+          </Link>
+        </p>
       </div>
-      <div className="footer-childs">
-        <h1>WORKING HOURS</h1>
-        <p>Mon - Fri: 9:00AM - 9:00PM</p>
-        <p>Sat: 9:00AM - 19:00PM</p>
-        <p>Sun: Closed</p>
-      </div>
-      <div className="footer-childs">
-        <h1>SUBSCRIPTION</h1>
-        <p>Subscribe your Email address for latest news & announcements.</p>
-        <form className="footer-form" ref={form} onSubmit={sendEmail}>
-          <input
-            type="email"
-            placeholder="Enter Email Address"
-            name="user_email"
-          />
-          <input type="submit" value="Send" />
-        </form>
-        <div
-          style={{ display: isSent ? "flex" : "none" }}
-          className="confirm-cont"
-        >
-          <img src="/check.png" alt="" />
-          <p>Subscribed succesfully!</p>
-        </div>
-      </div>
-      <p className="copy">©2024, EMSA. All rights reserved.</p>
     </div>
   );
 };
