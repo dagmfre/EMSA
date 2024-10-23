672b368609ab096665755a42fa83fe4450e75f3e
dagmfre <dagmfre@gmail.com>
2024-10-24 00:49:39 +0300

    Refactor image source paths and add Suspense for lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { ParallaxProvider } from "react-scroll-parallax";
 import ScrollToTop from "./scrollToTop";  
 import routes from "./routes";  
+import Loader from "./Loader";
 
 function App() {
   useEffect(() => {
@@ -14,11 +15,13 @@ function App() {
   return (
     <ParallaxProvider>
       <ScrollToTop />  
-      <Routes>
-        {routes.map(({ path, element: Component }, index) => (
-          <Route key={index} path={path} element={<Component />} />
-        ))}
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {routes.map(({ path, element: Component }, index) => (
+            <Route key={index} path={path} element={<Component />} />
+          ))}
+        </Routes>
+      </Suspense>
     </ParallaxProvider>
   );
 }
