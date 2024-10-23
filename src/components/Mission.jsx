3cf6b332e0de6f9a102417f6db4719090e83b0b0
dagmfre <dagmfre@gmail.com>
2024-10-24 00:56:06 +0300

    Refactor Mission component to update mission statement and add animations

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -3,16 +3,25 @@ export default function Mission() {
     <div className="mission">
       <div className="mission-topic">
         <div>
-          <h1 data-aos-once="true" data-aos="fade-right" data-aos-duration="800">
+          <h1
+            data-aos-once="true"
+            data-aos="fade-right"
+            data-aos-duration="800"
+          >
             Our <span>Mission</span>
           </h1>
-          <p data-aos-once="true" data-aos="fade-right" data-aos-duration="1200">
-            Lorem ipsum dolor sit amet, consectetuer.
+          <p
+            data-aos-once="true"
+            data-aos="fade-right"
+            data-aos-duration="1200"
+          >
+            To empower, develop, and unite medical students across Ethiopia.
           </p>
         </div>
 
         <img
-          data-aos-once="true" data-aos="fade-right"
+          data-aos-once="true"
+          data-aos="fade-right"
           data-aos-duration="1600"
           src="ethio_med.png"
           className="ethio-med-img"
@@ -25,10 +34,18 @@ export default function Mission() {
             <img src="/empower2.png" alt="" />
           </div>
           <div className="mission-main">
-            <h1 data-aos-once="true" data-aos="fade-left" data-aos-duration="800">
+            <h1
+              data-aos-once="true"
+              data-aos="fade-left"
+              data-aos-duration="800"
+            >
               Empowerment
             </h1>
-            <p data-aos-once="true" data-aos="fade-left" data-aos-duration="1000">
+            <p
+              data-aos-once="true"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+            >
               Become a platform for the personal and professional growth and
               development of medical students all over Ethiopia by providing
               opportunities to expand their horizons and work on solutions for
@@ -42,15 +59,25 @@ export default function Mission() {
         </div>
         <div className="missions">
           <div className="mission-emoji">
-            <i class="fa-solid fa-lightbulb"></i>
+            <i className="fa-solid fa-lightbulb"></i>
           </div>
           <div className="mission-main">
-            <h1 data-aos-once="true" data-aos="fade-left" data-aos-duration="1200">Development</h1>
-            <p data-aos-once="true" data-aos="fade-left" data-aos-duration="1400">
+            <h1
+              data-aos-once="true"
+              data-aos="fade-left"
+              data-aos-duration="1200"
+            >
+              Development
+            </h1>
+            <p
+              data-aos-once="true"
+              data-aos="fade-left"
+              data-aos-duration="1400"
+            >
               Creating an environment in which medical students are
               well-equipped with all the necessary knowledge and skills so that
               they may be influential in the betterment of the healthcare system
-              both locally and glob
+              both locally and globally.
             </p>
           </div>
         </div>
@@ -59,12 +86,22 @@ export default function Mission() {
             <img src="/connect2.png" alt="" />
           </div>
           <div className="mission-main">
-            <h1 data-aos-once="true" data-aos="fade-left" data-aos-duration="1300">Unity</h1>
-            <p data-aos-once="true" data-aos="fade-left" data-aos-duration="1500">
-              Connect, srightport and stand for medical students all over
-              Ethiopia in their journey of acquiring medical knowledge and
-              develop their all rounded skills and become a platform for them to
-              serve the community and achieve their goals.
+            <h1
+              data-aos-once="true"
+              data-aos="fade-left"
+              data-aos-duration="1300"
+            >
+              Unity
+            </h1>
+            <p
+              data-aos-once="true"
+              data-aos="fade-left"
+              data-aos-duration="1500"
+            >
+              Connect, support and stand for medical students all over Ethiopia
+              in their journey of acquiring medical knowledge and develop their
+              all rounded skills and become a platform for them to serve the
+              community and achieve their goals.
             </p>
           </div>
         </div>
